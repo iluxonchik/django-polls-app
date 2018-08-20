63f458be744f3c97533653d47c2c1373043f8397
ILUXONCHIK <illya@iluxonchik.me>
2018-08-20 04:55:30 +0100

    add urlpatterns and view; part 1 completed

--- django_refresher/django_refresher/urls.py
+++ django_refresher/django_refresher/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+"""
+Notes
+-----------------------------------------------------------------------------
+
+* include() - reference other URLconfs
+    Whenever Django encounters an include(), it strips out whatever part
+    of the url was matched up to that point, and sends the remainder to the
+    included URLconf. Note, how we're not dispatching a vew function here,
+    but rather passing the remainder of the URLs string for further matching.
+
+"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    # NOTE: if the polls app was under /content/polls/, fun_polls/, it would still work
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
