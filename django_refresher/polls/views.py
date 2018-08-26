326f115b7082cb91f807ca0961c0a1115588bf7c
ILUXONCHIK <illya@iluxonchik.me>
2018-08-26 22:23:26 +0100

    convert views to generic views; part 4 completed

--- django_refresher/polls/views.py
+++ django_refresher/polls/views.py
@@ -3,6 +3,23 @@ from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DeatilView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def index_not_optimized(request):
     """
@@ -59,7 +76,8 @@ def results(request, question_id):
                   'polls/results.html',
                   {
                       'question': question,
-                  })
+                  }
+                  )
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
